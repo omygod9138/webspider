dc93247dfa3df5bb21e8beaced4b63817cf0bffd
John <omygod9138@users.noreply.github.com>
2016-04-14 16:38:47 +0800

    Update webspider.php
    1. removed [attr] option in settings
    2. added [downLoad] option in settings
    3. added [depth] option in settings
    4. changed the way get_tag function formats html tags for searching

--- webspider.php
+++ webspider.php
@@ -3,43 +3,61 @@
 	myWebspider
 	
 	@param $obj : object settings for crawling
-			url : the url to crawl			| string
-			keyword: the series to find		| array
-			tag : specific tag to find		| string
-			class : specific class to find  | array
-			page : no of page to crawl		| int
-			attr : attr of tag to find		| array
-			record: name of file to record which files have been downloaded | string
+		base_url : url to use for concating | string        
+		url : the url to crawl          | string
+		keyword: keyword to find        | array
+		tag : specific tag to find      | array
+		downLoad_tag : specific download tag    | array
+		page : no of page to crawl      | int
+		depth : how deep to crawl       | int
+		pagination : attr to get pagination | array
+		regexp : for filter strings     | string
+		record: name of file to record which files have been downloaded | string
+		action: just get link or download(only torrent) | string
 			
 */
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ob_implicit_flush(true);
+ob_end_flush();
 class WebSpider{
 	private $settings = array();
 	private $page_count = 1;
 	public function __construct($obj){
-		
 		include_once('simple_html_dom.php');
 		set_time_limit(0);
 		$this->setVar($obj);
 		$this->links_found = array();
-
+		if( ! is_writable(dirname("../webSpider"))){
+			echo "no perm <br>".PHP_EOL;
+			chown("../webSpider", 0755);
+		}
 		if(file_exists($this->settings['record']."_record.txt")){
+			if(is_writable($this->settings['record']."_record.txt")){
+				echo "file can write <br>".PHP_EOL;
+			}
 			$this->episode = json_decode(file_get_contents($this->settings['record']."_record.txt"), true);
+			
 		}else{
 			$this->episode = array();
 		}
 		
+		if($this->settings['page'] > 1){
+			$html = $this->get_html($this->settings['url']);
+			$this->getPage($html);
+		}
 		if(!file_exists("dl") && !is_dir("dl")){
 			mkdir("dl");         
 		} 
 		
 	}
 	public function __destruct(){
-		$fp = fopen ($this->settings['record']."_record.txt", "w");
+		$fp = fopen($this->settings['record']."_record.txt", "w")or die("can't open file");;
 		$log = json_encode($this->episode);
+		echo "saving record to ".$this->settings['record']."_record.txt...<br>".$log.PHP_EOL;
 		fwrite($fp, $log);
-		fclose($fp); 
+		fclose($fp);
+		
 	}
 	
 	public function get_html($url = NULL){
@@ -59,55 +77,55 @@ class WebSpider{
 			return $returned;
 		}
 	}
-	
+	/*
+		find the links of current page
+	*/
 	public function analyze_html($html){
 		echo "start analyzing html...".PHP_EOL;
+		if($this->settings['page'] > 1){
+			$this->page_count ++;
+			echo "refresh pagination...".PHP_EOL;
+			$this->getPage($html);
+		}
 		$html = str_get_html($html);
-		$tag = $this->get_tag(0);
+		$tag = $this->get_tag($this->settings['tag']);
+		echo "tag to match ".$tag.PHP_EOL;
 		foreach($html->find($tag) as $link)
 		{
-			
 			$str = strtolower($link->innertext);
+			echo $str.PHP_EOL;
 			foreach($this->settings['keyword'] AS $title)
 			{
 				//echo "matching ".$title."<br>";
 				if(strpos(strtolower($str), $title) !== false)
 				{
-					$season = $this->settings['regexp'];
-					preg_match($season, $str, $season_ary);
-					if(!array_key_exists($title, $this->episode)){
-						$this->episode[$title] = array();
+					//echo $str.PHP_EOL;
+					if($this->settings['regexp'] != NULL){
+						$season = $this->settings['regexp'];
+						preg_match($season, $str, $season_ary);
+						if(empty($this->episode) || !array_key_exists($title, $this->episode)){
+							$this->episode[$title] = array();
+							echo "array key added <br>".PHP_EOL; 
+						}
+						
+						if(isset($season_ary[0]) && !in_array($season_ary[0], $this->episode[$title])){
+							echo "href...".$link->href;
+							echo "<br>".PHP_EOL;
+							$this->episode[$title][] = $season_ary[0];
+							$this->links_found[$link->innertext] = $link->href;
+							$this->write_log("[".date("Y-m-d H:i:s")."] episode - ".$link->innertext." added ".PHP_EOL, 'log.txt');
+						}
+						
+						break;
+					}else{
+						echo 'href found '.$link->href,PHP_EOL;
+						$file = date("Y-m-d").".txt";
+						if(!file_exists($file)){
+							fopen($file, "w");
+							//fclose($file);
+						}
+						$this->write_log("[".date("Y-m-d H:i:s")."] episode - ".$link->innertext." added ".PHP_EOL, $file);
 					}
-					
-					if($season_ary[0] != NULL && !in_array($season_ary[0], $this->episode[$title])){
-						echo $season_ary[0]." not null ".PHP_EOL;
-						$this->episode[$title][] = $season_ary[0];
-						$this->links_found[$link->innertext] = $link->href;
-						$this->write_log("[".date("Y-m-d H:i:s")."] episode - ".$link->innertext." added ".PHP_EOL, 'log.txt');
-					}
-					/*
-					$season = "/[sS][0-9][0-9]/";
-					$ep = "/[eE][0-9][0-9]/";
-					preg_match($season, $str, $season_ary);
-					preg_match($ep, $str, $ep_ary);
-					$s = substr($season_ary[0], 1, strlen($season_ary[0]));
-					$e = substr($ep_ary[0], 1, strlen($ep_ary[0])); 
-					if(!array_key_exists($title, $this->episode)){
-						$this->episode[$title] = array();
-					}
-					
-					if(!array_key_exists($s, $this->episode[$title])){
-						$this->episode[$title][$s] = array($e);
-					}
-					
-					if(!in_array($e, $this->episode[$title][$s])){
-						$this->episode[$title][$s][] = $e;
-						$this->links_found[$link->innertext] = $link->href;
-						$this->write_log("[".date("Y-m-d H:i:s")."] episode - ".$link->innertext." added ".PHP_EOL, 'log.txt');
-					}
-					*/
-					break;
-					
 				}
 				
 			}
@@ -117,12 +135,17 @@ class WebSpider{
 		return count($this->links_found);
 		
 	}
-	
+	/*
+		starting point of crawler
+	*/
 	public function start_crawling(){
-		
-		$url = ($this->page_count > 1) ? $this->settings['url'].$this->page_count."/" : $this->settings['url'];
-		echo "crawling ".$url.PHP_EOL;
+		//if pages to crawl > 1 set url to next page
+		echo 'start crawling...page '.$this->page_count.PHP_EOL;
+		$url = ($this->page_count > 1) ? html_entity_decode($this->settings['base_url'].$this->pagination[$this->page_count]) : $this->settings['url'];
+		echo 'url....'.$url.PHP_EOL;
+		//get the html from url given
 		$html = $this->get_html($url);
+		
 		if($this->analyze_html($html) > 0){
 			
 			echo "new list found ".PHP_EOL;
@@ -131,9 +154,7 @@ class WebSpider{
 		}else{
 			
 			if($this->settings['page'] > 0 && $this->settings['page'] > $this->page_count){
-				
-				$this->page_count ++;
-				echo "start crawling page ".$this->page_count." ".PHP_EOL;
+				echo "crawling next page".PHP_EOL;
 				$this->start_crawling();
 			
 			}else{
@@ -144,12 +165,13 @@ class WebSpider{
 				$this->start_crawling();
 			}
 		}
+		
 	}
 	
 	public function getData(){
+		
 		echo "fetching data...".PHP_EOL;
 		if($this->settings['page'] > 0 && $this->settings['page'] > $this->page_count){
-			$this->page_count++;
 			$this->start_crawling();
 		}
 		$func = $this->settings['action'];
@@ -167,14 +189,11 @@ class WebSpider{
 	public function downLoad($link, $file){
 		echo "directing to download page ".PHP_EOL;
 		$download_link = $this->settings['base_url'].$link;
-		//echo "file name - ".$file."<br>";
 		$returned = $this->get_html($download_link);
 		$download_page = str_get_html($returned);
 		$path = 'dl/'.$file.'.torrent';
-		//echo "file name - ".$path."<br>";
-		$tag = $this->get_tag(1);
+		$tag = $this->get_tag($this->settings['downLoad_tag']);
 		$dlink = $download_page->find($tag)[0];
-		//echo "link - ".$dlink->href."<br/>";
 		echo "text - ".$file.PHP_EOL;
 		$url = "https:".$dlink->href;
 		if(!$this->copySecureFile($url, $path)){
@@ -183,27 +202,42 @@ class WebSpider{
 	}
 	
 	public function getLink($link, $file){
-		echo "getLink...".PHP_EOL;
-		$_link = $this->settings['base_url'].$link;
-		$returned = $this->get_html($_link);
-		$_page = str_get_html($returned);
-		$tag = $this->get_tag(1);
-		$dlink = $_page->find($tag)[0];
+		echo "getLink...".$link.PHP_EOL;
 		$filename =  $this->getFilename($this->settings['keyword'], $file);
-		$href = $dlink->href;
-		$str = "[".date("Y-m-d H:i:s")."]".$file." ".$href.PHP_EOL;
-		$this->write_log($str, $filename);
+		$_link = $this->settings['base_url'].$link;
+		if($this->settings['depth'] > 1){
+			$returned = $this->get_html($_link);
+			$_page = str_get_html($returned);
+			$tag = $this->get_tag($this->settings['downLoad_tag']);
+			$dlink = $_page->find($tag)[0];
+			$href = $dlink->href;
+			$str = "[".date("Y-m-d H:i:s")."]".$file." ".$href.PHP_EOL;
+		}else{
+			$str = "[".date("Y-m-d H:i:s")."]".$file." ".$_link.PHP_EOL;
+		}
+		$this->write_log($str, "log_[".date("Y-m-d")."].txt");
+	}
+	
+	public function getPage($html){
+		echo 'get page...'.$this->page_count.PHP_EOL;
+		unset($this->pagination);
+		$this->pagination = array();
+		$html = str_get_html($html);
+	
+		foreach($html->find($this->settings['pagination_attr']) AS $page){
+			$this->pagination[$page->innertext] = $page->href;
+		}
+		
+		return array_filter($this->pagination);
+		
 	}
 	
 	public function getFilename($keywords, $file){
-		//echo gettype($keywords).$keywords.PHP_EOL;
+		
 		foreach($keywords As $keyword){
-			//echo "file ".$file.PHP_EOL;
-			//echo "filename ".$keyword.'.txt'.PHP_EOL;
+			
 			if(strpos("bla ".strtolower($file), $keyword) !== false){
-				//echo PHP_EOL;
-				//echo "file ".$file.PHP_EOL;
-				//echo "filename ".$keyword.".txt".PHP_EOL;
+				
 				return $keyword.".txt";
 			}
 		}
@@ -222,20 +256,36 @@ class WebSpider{
 	public function write_log($str, $filename){
 		
 		echo 'logging new log '.$str.PHP_EOL;
-		
 		$fp  = file_get_contents($filename);
 		$fp .= $str;
 		file_put_contents($filename, $fp);
 		
+		
 	}
 	
-	public function get_tag($index){
-		return $this->settings['tag'].'['.$this->settings['col'][$index].'='.$this->settings['attr'][$this->settings['col'][$index]].']';
+	public function get_tag($ary){
+		$str = '';
+		foreach($ary AS $element){
+			
+			$str .= $element['tag']."[";
+			if(count($element['attr']) > 1){
+				$str .= $element['attr'][0]."=".$element['attr'][1]."] ";
+				
+			}else{
+				$str .= $element['attr'][0]."] ";
+			}
+		}
+		return trim($str);
+	
 	}
 	
 	public function setVar($obj){
 		$this->settings = $obj;
 		$this->settings['col'] = array_keys($this->settings['attr']);
-		$this->settings['current_depth'] = 1;
+		if(!isset($this->settings['depth']))
+		{
+			$this->settings['depth'] = 1;
+		}
+		$this->settings['pagination_attr'] = $this->get_tag($this->settings['pagination']);
 	}
 }
